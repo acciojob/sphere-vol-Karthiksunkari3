f6394e5a1247b0ac158ee2a1c450d0b79e4cbba0
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-07-09 23:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+  event.preventDefault(); // Prevents the form from submitting and refreshing the page
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  // Get the radius value from the input field
+  var radius = parseFloat(document.getElementById('radius').value);
+
+  // Calculate the volume of the sphere
+  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  // Update the volume field with the calculated value
+  document.getElementById('volume').value = volume.toFixed(2);
+}
+
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+};
